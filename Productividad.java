5667d4a85cd06ec616d5bc03b81c026e4b107152
DiazMarco2005 <dia24229@uvg.edu.gt>
2024-11-09 22:07:56 -0600

    cambios en el radio y productividad metodos hechos

--- Productividad.java
+++ Productividad.java
@@ -1,17 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 class Productividad extends Modo {
     private Random random;
+    private ArrayList<String> climate;
 
     public Productividad() {
         this.nombre = "Productividad";
         this.random = new Random();
+        this.climate= new ArrayList<>(Arrays.asList("Soleado", "Nublado", "Chubuzcos", "LLuvia","Llovizna","Tormenta"));
+
     }
 
-    public void obtenerPronostico() {
-        //
+    public String obtenerPronostico() {
+        double min=-5.5;
+        double max=40;
+        double degree=min + (random.nextDouble() * (max - min));
+        String strdegre=Double.toString(degree);
+        String clima=climate.get(random.nextInt((climate.size())));
+        return "Hoy la temperatura es de "+strdegre+" con un tiempo: "+clima;
+
     }
 
     @Override
-    public void cambiarModo() {}
+    public void cambiarModo() {
+        this.radio.modo(this);
+    }
 }
--- Radio.java
+++ Radio.java
@@ -1,35 +1,82 @@
 import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
 
 class Radio extends Modo {
     private List<HashMap<String, Double>> frequencyList;
     private String banda;
     private double frecuencia;
 
+    public Radio(List<HashMap<String, Double>> frequencyList, String banda, double frecuencia) {
+        this.frequencyList = frequencyList;
+        this.banda = banda;
+        this.frecuencia = frecuencia;
+    }
+
     public Radio() {
-        this.nombre = "Radio";
-        this.frequencyList = new ArrayList<>();
-        this.banda = "FM";
-        this.frecuencia = 87.5;
     }
 
-    public void cambiarEmisora(double cambio) {
-        //
+    public List<HashMap<String, Double>> getFrequencyList() {
+        return frequencyList;
     }
 
-    public void cambiarBanda() {
-        //
+    public void setFrequencyList(List<HashMap<String, Double>> frequencyList) {
+        this.frequencyList = frequencyList;
     }
 
-    public void guardarEmisora() {
-        //
+    public String getBanda() {
+        return banda;
     }
 
-    public void cargarEmisora(int index) {
-        //
+    public void setBanda(String banda) {
+        this.banda = banda;
+    }
+
+    public double getFrecuencia() {
+        return frecuencia;
+    }
+
+    public void setFrecuencia(double frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+
+    public void cambiarEmisora(int add) {
+        double nuevaFrecuencia=0;
+    
+        if (add == 1) {
+            nuevaFrecuencia = this.frecuencia + 0.5;
+        } else if (add == -1) {
+            nuevaFrecuencia = this.frecuencia - 0.5;
+        } 
+
+        // Validación de rango, suponiendo que estamos en la banda FM
+        if (nuevaFrecuencia >= 87.5 && nuevaFrecuencia <= 108.0) {
+            setFrecuencia(nuevaFrecuencia);  // Asigna la nueva frecuencia si está en el rango
+        } else if(nuevaFrecuencia<=87.5) {
+            setFrecuencia(87.5);
+        }else if (nuevaFrecuencia>108.0){
+            setFrecuencia(108.0);
+        }
+
+    }
+    
+
+    public void guardarEmisora(String nombre ,double frecuencia) {
+        HashMap<String, Double> estacion = new HashMap<>();
+        estacion.put(nombre, frecuencia);  // Asigna el nombre y la frecuencia
+        frequencyList.add(estacion);
+    }
+    public void cargarEmisora(double index) {
+        for (HashMap<String,Double> hashMap : frequencyList) {
+            for(Double frecuencia: hashMap.values()){
+                if(frecuencia.equals(index)){
+                    setFrecuencia(index);
+                }
+            }
+        }
     }
 
     @Override
-    public void cambiarModo() {}
+    public void cambiarModo() {
+        this.radio.modo(this);
+    }
 }
